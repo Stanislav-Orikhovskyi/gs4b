78429af82a052c925c19c9ab5c2c12be003e4252
Stanislav <43533425+Soft-Scale@users.noreply.github.com>
2024-04-28 13:37:16 +0300

    Додано верстку сторÑAdded new progectue.js тand Vuetify

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app >
+    <main class="container">
+
+      <v-main>
+
+      </v-main>
+
+      <v-breadcrumbs :items="['Главная', 'Купить', 'Автомобили Ford', 'Ford Edge 2016 - Код товара: 40845']">/</v-breadcrumbs>
+    </main>
+  </v-app>
+</template>
+
+<script setup>
+  //
+</script>
